40c7b67b733a45e99ac4c820f9d4202129783f74
REYE237 <lily.reyes@arvato-scs.com>
2021-07-09 16:18:29 +0200

    Assignment on data binding

--- src/app/components/servers/servers.component.ts
+++ src/app/components/servers/servers.component.ts
@@ -2,30 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // selector: '[app-servers]',
-  // selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
-  serverCreationStatus='No server was created!';
-  serverName='testServer';
+  username='';
 
   constructor() {
-    setTimeout(() => {
-      this.allowNewServer=true;
-    },2000);
+
   }
 
   ngOnInit(): void {
   }
 
-  onCreateServer(){
-    this.serverCreationStatus='Server was created! Name is ' + this.serverName;
-  }
 
-  onUpdateServerName(event:any){
-    this.serverName= event.target.value;
+
+  onResetUsername(event:any){
+    this.username='';
   }
 }
