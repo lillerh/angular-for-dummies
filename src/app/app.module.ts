6a4bc15a986ad71d654dbdb3d906e67b37ef3653
REYE237 <lily.reyes@arvato-scs.com>
2021-07-09 16:24:38 +0200

    Cleansing code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './components/server/server.component';
-import { ServersComponent } from './components/servers/servers.component';
-import { SuccessAlertComponent } from './components/success-alert/success-alert.component';
-import { WarningAlertComponent } from './components/warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
