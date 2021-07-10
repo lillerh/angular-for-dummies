9746ecc759748dd2ee349a78aad1f91f8a0be4a3
REYE237 <lily.reyes@arvato-scs.com>
2021-07-10 14:23:41 +0200

    Extracting index when using ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   buttonClicked=false;
-  log :number[]= [];
+  log : Date[]= [];
 
   onDisplayDetails(){
     this.buttonClicked= !this.buttonClicked;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date())
   }
 
    getDate(){
